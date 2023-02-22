9178efabec2291af8189a3de24fd25a5cb0ec210
Lucas <lucascignacio@gmail.com>
2023-02-21 23:18:21 -0300

    Add Page Plans-show

---
+++ resources/views/admin/pages/plans/show.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', "Detalhes do plano {$plan->name}")
+
+@section('content_header')
+    <h1> Detalhes do plano <b>{{$plan->name}}</b></h1>
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <ul>
+                <li>
+                    <strong>Nome: </strong> {{$plan->name}}
+                </li>
+                <li>
+                    <strong>URL: </strong> {{$plan->url}}
+                </li>
+                <li>
+                    <strong>Preço: </strong> {{number_format($plan->price, 2, ',', '.')}}
+                </li>
+                <li>
+                    <strong>Descrição: </strong> {{$plan->description}}
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
