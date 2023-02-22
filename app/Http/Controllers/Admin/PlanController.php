430364624cff03ca8b86e9eeff8d7c64ba8b77fa
Lucas <lucascignacio@gmail.com>
2023-02-21 23:28:33 -0300

    Add destroy

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -50,4 +50,15 @@ class PlanController extends Controller
             'plan' => $plan
         ]);
     }
+
+    public function destroy($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+
+        if(!$plan)
+            return redirect()->back();
+        $plan->delete();
+
+        return redirect()->route('plans.index');
+    }
 }
