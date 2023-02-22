395d06ae7a1912bd1624473dc0724a1c1e0792b2
Lucas <lucascignacio@gmail.com>
2023-02-22 16:08:15 -0300

    Route plans edit/update

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index')
 Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
 Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
 Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
+Route::get('admin/plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
+Route::put('admin/plans/{url}', [PlanController::class, 'update'])->name('plans.update');
 Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
 Route::any('admin/plans/search', [PlanController::class, 'search'])->name('plans.search');
+
+Route::get('admin')->name('admin.index');
 
 Route::get('/', function () {
     return view('welcome');
