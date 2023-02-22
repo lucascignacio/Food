6c1f86c846c35369488ab33ea45734b6355f875d
Lucas <lucascignacio@gmail.com>
2023-02-22 01:11:04 -0300

    Add value Search

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -9,7 +9,11 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            #filtros
+            <form action="{{ route('plans.search') }}" method="POST" class="form form-inline">
+                @csrf
+                <input type="text" name="filter" placeholder="Nome" class="form-control" value="{{ $filters['filter'] ?? '' }}">
+                <button type="submit" class="btn btn-dark">Filtrar</button>
+            </form>
         </div>
         <div class="card-bory">
             <table class="table table-condensed">
@@ -38,7 +42,11 @@
             </table>
         </div>
         <div class="card-footer">
-            {!! $plans->links() !!} 
+            @if (isset($filters))
+                {!! $plans->appends($filters)->links() !!} 
+            @else 
+                {!! $plans->links() !!}
+            @endif
         </div>
     </div>
 @endsection
