9773e44356ae5f3f886afa1b7a333524d2051584
Lucas <lucascignacio@gmail.com>
2023-02-22 16:52:27 -0300

    Relacionamento polimorfico

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Models\DetailPlan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,11 @@ class Plan extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'url', 'price', 'description'];
+
+    public function details()
+    {
+        return $this->hasMany(DetailPlan::class);
+    }
 
     public function search($filter = null)
     {
