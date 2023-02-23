79def64c39bab33f11861152f553c8b5ffe04334
Lucas <lucascignacio@gmail.com>
2023-02-23 16:23:50 -0300

    Correção

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Models\DetailPlan;
+use App\Models\DetailPlan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
