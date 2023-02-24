3744dbe9aabbce8fd33ad4483a5a552e2c2bbb49
Lucas <lucascignacio@gmail.com>
2023-02-24 17:29:54 -0300

    Add Route Permission

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ACL\PermissionController;
 use App\Http\Controllers\Admin\ACL\ProfileController;
 use App\Http\Controllers\Admin\DetailPlanController;
 use App\Http\Controllers\Admin\PlanController;
@@ -16,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::prefix('admin')->group(function(){
+
+    /**
+     * Router Permissions
+     */
+    Route::any('permissions/search', [PermissionController::class, 'search'])->name('permissions.search');
+    Route::resource('permissions', PermissionController::class);
 
     /**
      * Routes Profiles
