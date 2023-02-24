0c5a9f1cbf04aa9dce4de8d29af076431a1bce7c
Lucas <lucascignacio@gmail.com>
2023-02-24 01:00:57 -0300

    Add create/update

--- app/Http/Controllers/Admin/DetailPlanController.php
+++ app/Http/Controllers/Admin/DetailPlanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\StoreUpdateDetailPlan;
 use App\Http\Controllers\Controller;
 use App\Models\DetailPlan;
 use App\Models\Plan;
@@ -30,4 +31,87 @@ class DetailPlanController extends Controller
             'details' => $details,
        ]);
     }
+
+    public function create($urlPlan)
+    {
+        if (!$plan = $this->plan->where('url', $urlPlan)->first()){
+            return redirect()->back();
+        }
+
+        return view('admin.pages.plans.details.create', [
+            'plan' => $plan
+        ]);
+    }
+
+    public function store(StoreUpdateDetailPlan $request, $urlPlan)  
+    {
+        if (!$plan = $this->plan->where('url', $urlPlan)->first()){
+            return redirect()->back();
+        }
+
+        $plan->details()->create($request->all());
+
+        return redirect()->route('details.plan.index', $plan->url);
+    }
+
+    public function edit($urlPlan, $idDetail)
+    {
+        $plan = $this->plan->where('url', $urlPlan)->first();
+        $detail = $this->repository->find($idDetail);
+
+        if(!$plan || !$detail) {
+            return redirect()->back();
+        }
+
+        return view('admin.pages.plans.details.edit', [
+            'plan' => $plan,
+            'detail' => $detail,
+        ]);
+    }
+
+    public function update(StoreUpdateDetailPlan $request, $urlPlan, $idDetail)  
+    {
+        $plan = $this->plan->where('url', $urlPlan)->first();
+        $detail = $this->repository->find($idDetail);
+
+        if(!$plan || !$detail) {
+            return redirect()->back();
+        }
+
+        $detail->update($request->all());
+
+        return redirect()->route('details.plan.index', $plan->url);
+    }
+
+    public function show($urlPlan, $idDetail)
+    {
+        $plan = $this->plan->where('url', $urlPlan)->first();
+        $detail = $this->repository->find($idDetail);
+
+        if(!$plan || !$detail) {
+            return redirect()->back();
+        }
+
+        return view('admin.pages.plans.details.show', [
+            'plan' => $plan,
+            'detail' => $detail,
+        ]);
+    }
+
+    public function destroy($urlPlan, $idDetail)  
+    {
+        $plan = $this->plan->where('url', $urlPlan)->first();
+        $detail = $this->repository->find($idDetail);
+
+        if(!$plan || !$detail) {
+            return redirect()->back();
+        }
+
+        $detail->delete();
+
+        return redirect()
+        ->route('details.plan.index', $plan->url)
+        ->with('message', 'Registro deletado com sucesso!');
+    }
+    
 }
