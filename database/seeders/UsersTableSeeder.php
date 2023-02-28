5c5145defcb28bd8fb9944cc680d8f30cee1e8b1
Lucas <lucascignacio@gmail.com>
2023-02-28 00:35:10 -0300

    Users Seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $tenant = Tenant::first();
+
+        $tenant->users()->create([
             'name' => 'Marcos Brás',
             'email' => 'marcosbras@gmail.com',
             'password' => bcrypt('123456'),
