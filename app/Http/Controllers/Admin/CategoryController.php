91095f007ed22d4e96bc6d0fd53cf9fe32e25392
Lucas <lucascignacio@gmail.com>
2023-03-01 00:44:48 -0300

    Category Controller

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategory;
+use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Htt;
+
+class CategoryController extends Controller
+{
+    private $repository;
+
+    public function __construct(Category $category)
+    {
+        $this->repository = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = $this->repository->latest()->paginate();
+
+        return view('admin.pages.categories.index', compact('categories'));
+    }
+
+    /** 
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.pages.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUpdateCategory $request)
+    {
+        $this->repository->create($request->all());
+
+        return redirect()->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
