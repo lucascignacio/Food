bddfed8386c6eafad64cfd2fbaaba98f19066eb2
Lucas <lucascignacio@gmail.com>
2023-03-01 20:16:07 -0300

    Request personalizado Product

---
+++ app/Http/Requests/StoreUpdateProduct.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateProduct extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        $id = $this->segment(3);
+
+        $rules = [
+            'title' => ['required', 'min:3', 'max:255', "unique:products,title,{$id},id"],
+            'description' => ['required', 'min:3', 'max:500'],
+            'image' => ['required', 'image'],
+        ];
+
+        if($this->method() == 'PUT') {
+            $rules['image'] = ['nullable', 'image'];
+        }
+
+        return $rules;
+    }
+}
