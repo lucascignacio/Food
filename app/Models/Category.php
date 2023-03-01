1795effb1c12dd4ab788d586a014917cd62fc2e2
Lucas <lucascignacio@gmail.com>
2023-03-01 12:59:07 -0300

    .

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Tenant\Observers\TenantObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Tenant\Traits\TenantTrait;
