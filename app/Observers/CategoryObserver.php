ce19b3275b91b4e86f95cbebdc44bcb4a71bd757
Lucas <lucascignacio@gmail.com>
2023-03-01 00:45:19 -0300

    Category Observer

---
+++ app/Observers/CategoryObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoryObserver
+{
+        /**
+     * Handle the Plan "creating" event.
+     */
+    public function creating(Category $category)
+    {
+        $category->url = Str::kebab($category->name);
+    }
+
+    /**
+     * Handle the Plan "updating" event.
+     */
+    public function updating(Category $category)
+    {
+        $category->url = Str::kebab($category->name);
+    }
+}
