782e5974f444bf46a94282f63efe5d868b38f3fb
Lucas <lucascignacio@gmail.com>
2023-03-02 00:18:13 -0300

    Change icon

--- config/adminlte.php
+++ config/adminlte.php
@@ -319,7 +319,7 @@ return [
         [
             'text'        => 'Produtos',
             'url'         => 'admin/products',
-            'icon'        => 'fas fa-layer-group'
+            'icon'        => 'fas fa-hamburger'
         ],
 
       
