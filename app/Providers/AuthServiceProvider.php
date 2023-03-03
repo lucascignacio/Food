794c8379a6170ddb87d619bddd8c3785ee255e8b
Lucas <lucascignacio@gmail.com>
2023-03-03 13:36:35 -0300

    Add permission for routes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Permission;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +24,24 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        $permissions = Permission::all();
+
+        foreach($permissions as $permission){
+            Gate::define($permission->name, function(User $user) use ($permission) {
+                return $user->hasPermission($permission->name);
+            });
+        }
+        
+        Gate::define('owner', function(User $user, $object){
+            return $user->id === $object->user_id;
+        });
+
+        Gate::before(function(User $user){
+            if($user->isAdmin()){
+                return true;
+            }
+        });
+
     }
 }
