070afa9696baea340c14e2a1ad16631896345a9c
Lucas <lucascignacio@gmail.com>
2023-03-07 22:12:40 -0300

    Add routes api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryApiController;
+use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Api\TableApiController;
+use App\Http\Controllers\Api\TenantApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/tenants/{uuid}', [TenantApiController::class, 'show']);
+Route::get('/tenants', [TenantApiController::class, 'index']);
+
+Route::get('/categories/{identify}}', [CategoryApiController::class, 'show']);
+Route::get('/categories', [CategoryApiController::class, 'categoriesByTenant']);
+
+Route::get('/tables/{identify}', [TableApiController::class, 'show']);
+Route::get('/tables', [TableApiController::class, 'tablesByTenant']);
+
+Route::get('/products', [ProductApiController::class, 'productsByTenant']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
