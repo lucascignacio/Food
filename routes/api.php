d889ace8080cfa3ba5ba0acbfd0288c6da588485
Lucas <lucascignacio@gmail.com>
2023-03-09 11:38:36 -0300

    Add token Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthClientController;
 use App\Http\Controllers\Api\CategoryApiController;
 use App\Http\Controllers\Api\ProductApiController;
 use App\Http\Controllers\Api\TableApiController;
 use App\Http\Controllers\Api\TenantApiController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/sanctum/token', [AuthClientController::class, 'auth']);
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function(){
+    Route::get('/auth/me', [AuthClientController::class, 'me']);
+    Route::post('/auth/logout', [AuthClientController::class, 'logout']);
+});
 
 Route::group(['prefix' => 'v1'], function(){
     
@@ -31,6 +41,8 @@ Route::group(['prefix' => 'v1'], function(){
 
     Route::get('/products/{flag}', [ProductApiController::class, 'show']);
     Route::get('/products', [ProductApiController::class, 'productsByTenant']);
+
+    Route::post('/client', [RegisterController::class, 'store']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
