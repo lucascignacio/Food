ab37b12562394f1f5b1aa4241ebe051acb1cde9a
Lucas <lucascignacio@gmail.com>
2023-03-10 14:28:13 -0300

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            TenantsTableSeeder::class,
             PlansTableSeeder::class,
+            TenantsTableSeeder::class,
             UsersTableSeeder::class,
         ]);
     }
