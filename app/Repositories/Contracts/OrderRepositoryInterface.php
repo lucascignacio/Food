c576a1107bb0c5229c86dfb162343757d8787d18
Lucas <lucascignacio@gmail.com>
2023-03-14 21:05:36 -0300

    Order Functions

--- app/Repositories/Contracts/OrderRepositoryInterface.php
+++ app/Repositories/Contracts/OrderRepositoryInterface.php
@@ -14,4 +14,5 @@ interface OrderRepositoryInterface
         $tableId = ''
     );
     public function getOrderByIdentify(string $identify);
+    public function registerProductsOrder(int $orderId, array $products);
 }
