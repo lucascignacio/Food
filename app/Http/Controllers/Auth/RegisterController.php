80467183fd6f3553bf37356723e2c924d905a502
Lucas <lucascignacio@gmail.com>
2023-03-17 15:30:10 -0300

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -57,7 +57,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'min:3', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:5', 'confirmed'],
             'empresa' => ['required', 'string', 'min:3', 'max:255', 'unique:tenants,name'],
-            'cnpj' => ['required', 'numeric', 'min:10', 'unique:tenants']
+            'cnpj' => ['required', 'numeric', 'digits:14', 'unique:tenants']
         ]);
     }
 
